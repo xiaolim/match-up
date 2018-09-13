5d448dfaa9207c1411e7962b1e6bc93377ab18a1
wolszhang <wolszhang@gmail.com>
2018-09-13 13:46:32 -0400

    use the implemented permutation function to find the best lineup

--- matchup/g7/Player.java
+++ matchup/g7/Player.java
@@ -13,12 +13,14 @@ public class Player implements matchup.sim.Player {
 	private List<List<Integer>> distribution;
 	private List<Float> averageStrength;
 	private List<Integer> availableRows;
-
+	private boolean state;
 	public Player() {
 		//skills = new ArrayList<Integer>();
 		// TODO Find out a good skill set
 		skills = new ArrayList<Integer>(Arrays.asList(1, 1, 2, 3, 4, 5, 6, 6, 7, 8, 8, 9, 9, 10, 11));
 		availableRows = new ArrayList<Integer>(Arrays.asList(0, 1, 2));
+		averageStrength = new ArrayList<Float>();
+		state = false;
 	}
 	
 	@Override
@@ -34,6 +36,7 @@ public class Player implements matchup.sim.Player {
 	@Override
 	public List<List<Integer>> getDistribution(List<Integer> opponentSkills, boolean isHome) {
 		// TODO Come up with a way to form distribution against opponentSkills and the team position
+		state = isHome;
 		distribution = new ArrayList<List<Integer>>();
 		if (isHome) {
 			distribution.add(new ArrayList<Integer>(Arrays.asList(11, 10, 8, 6, 4)));
@@ -45,7 +48,10 @@ public class Player implements matchup.sim.Player {
 			distribution.add(new ArrayList<Integer>(Arrays.asList(8, 8, 6, 5, 4)));
 			distribution.add(new ArrayList<Integer>(Arrays.asList(6, 3, 2, 1, 1)));
 		}
-		
+		for (int i=0; i<distribution.size(); i++){
+			averageStrength.add(findAverage(distribution.get(i)));
+		}
+
 		return distribution;
 	}
 	
@@ -74,7 +80,6 @@ public class Player implements matchup.sim.Player {
 	private Pair<Integer, List<Integer>> permutation(int row, List<Integer> opponentRound) {
 		// TODO Assigned to Warren
 		ArrayList<ArrayList<Integer>> all_possible = new ArrayList<ArrayList<Integer>>();
- 		
  		all_possible.add(new ArrayList<Integer>());
  		List<Integer> line = distribution.get(row);
 		for (int i = 0; i < line.size(); i++) {
@@ -93,10 +98,8 @@ public class Player implements matchup.sim.Player {
 				}
 			}
 			all_possible = new ArrayList<ArrayList<Integer>>(current);
-
 		}
-
-		int best_score = 0;
+		int best_score = -6;
 		List<Integer> best_lineup = line;
 		for (int i=0; i<all_possible.size();i++){
 			if (ComputeScore(all_possible.get(i),opponentRound)>best_score){
@@ -106,9 +109,6 @@ public class Player implements matchup.sim.Player {
 		}
 		return new Pair<Integer, List<Integer>>(best_score,best_lineup); 
 	}
-
-
-
 
 	private int ComputeScore(List<Integer> line1, List<Integer> line2){
 		int score = 0;
@@ -123,20 +123,38 @@ public class Player implements matchup.sim.Player {
 		return score;
 	}
 
-
-	protected int useRows(){
-		// TODO
-		int k = availableRows.get(0);
-		availableRows.remove(0);
-		return k;
+	protected Pair<Integer, List<Integer>> useRows(List<Integer> opponentRound){
+		int best_line = availableRows.get(0);
+		int best_score = permutation(availableRows.get(0),opponentRound).getKey();
+		List <Integer> line_up = permutation(availableRows.get(0),opponentRound).getValue();
+		for (int i=1; i<availableRows.size(); i++){
+			int index = availableRows.get(i);
+			int cur_score = permutation(index,opponentRound).getKey();
+			List <Integer> cur_line_up = permutation(index,opponentRound).getValue();
+			if (cur_score > best_score && averageStrength.get(index) > averageStrength.get(best_line)){
+				best_line = index;
+				best_score = cur_score;
+				line_up = cur_line_up;
+			}
+		}
+		//System.out.println(best_score);
+		return new Pair<Integer, List<Integer>>(best_line, line_up);
 	}
 
-	
 	@Override
 	public List<Integer> playRound(List<Integer> opponentRound) {
 		// TODO Assigned to Will
-    	List<Integer> round = new ArrayList<Integer>(distribution.get(availableRows.get(0)));
-    	availableRows.remove(0);
+		List<Integer> round = new ArrayList<Integer>();
+
+    	if (state){
+    		Pair<Integer, List<Integer>> temp = useRows(opponentRound);
+    		round = temp.getValue();
+    		availableRows.remove(temp.getKey());
+    	}
+    	else{
+	    	round =	distribution.get(availableRows.get(0));
+    		availableRows.remove(0);
+    	}
 		return round;
 	}
 
