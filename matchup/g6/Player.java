7646c4359c7378a11354d2442b223f16d15f10e0
Jincheng Zhang <zhjch05@live.com>
2018-09-18 12:07:49 -0400

    learning strategy helper

--- matchup/g6/Player.java
+++ matchup/g6/Player.java
@@ -27,12 +27,12 @@ public class Player implements matchup.sim.Player {
 
     public List<Integer> getSkills() {
         // skill distribution modeled after Group 5
-        for (int i=0; i<3; ++i) {
+        for (int i = 0; i < 3; ++i) {
             skills.add(9); //three 9s
             skills.add(8); //three 8s
             skills.add(1); //three 1s
         }
-        for (int i=0; i<2; ++i) {
+        for (int i = 0; i < 2; ++i) {
             skills.add(7); //two 7s
             skills.add(6); //two 6s
             skills.add(5); //two 5s
@@ -65,25 +65,24 @@ public class Player implements matchup.sim.Player {
         return distribution;
     }
 
-    public int chooseLine(List<Integer> opponentRound){
+    public int chooseLine(List<Integer> opponentRound) {
         List<Double> ratio = new ArrayList<Double>();
-        for (int i =0; i < availableRows.size(); i++){
+        for (int i = 0; i < availableRows.size(); i++) {
             List<Integer> round = new ArrayList<Integer>(distribution.get(availableRows.get(i)));
-            round = optimizedRound(opponentRound,round);
+            round = optimizedRound(opponentRound, round);
             double tempScore = 0;
             double tempSkills = 90;
-            for (int j=0; j<5;j++){
-                if (round.get(j)-opponentRound.get(j)>2){
-                    tempScore+=1;
+            for (int j = 0; j < 5; j++) {
+                if (round.get(j) - opponentRound.get(j) > 2) {
+                    tempScore += 1;
+                } else if (opponentRound.get(j) - round.get(j) > 2) {
+                    tempScore -= 1;
                 }
-                else if (opponentRound.get(j)-round.get(j)>2){
-                    tempScore-=1;
-                }
-                tempSkills+=round.get(j);
-                tempSkills-=opponentRound.get(j);
+                tempSkills += round.get(j);
+                tempSkills -= opponentRound.get(j);
             }
             //System.out.println(tempScore/tempSkills);
-            ratio.add(tempScore/tempSkills);
+            ratio.add(tempScore / tempSkills);
         }
         double max = Collections.max(ratio);
         int index = ratio.indexOf(max);
@@ -93,10 +92,9 @@ public class Player implements matchup.sim.Player {
 
     public List<Integer> playRound(List<Integer> opponentRound) {
         int n = 0;
-        if (opponentRound!=null){
+        if (opponentRound != null) {
             n = chooseLine(opponentRound);
-        }
-        else{
+        } else {
             n = rand.nextInt(availableRows.size());
         }
 
@@ -107,7 +105,7 @@ public class Player implements matchup.sim.Player {
 
         /*** addition to Random ***/
         // If home, Run optimized permutation search given opponentRound instead of using random shuffle
-        if (opponentRound!=null) {
+        if (opponentRound != null) {
             round = optimizedRound(opponentRound, round);
         }
 
@@ -121,23 +119,23 @@ public class Player implements matchup.sim.Player {
 
 
     /*** custom helper functions ***/
-    private void printList(List<Integer> myList){
+    private void printList(List<Integer> myList) {
         System.out.print("[");
-        for(int i=0;i<myList.size()-1;i++){
-            System.out.print(myList.get(i)+", ");
+        for (int i = 0; i < myList.size() - 1; i++) {
+            System.out.print(myList.get(i) + ", ");
         }
-        System.out.print(myList.get(myList.size()-1));
+        System.out.print(myList.get(myList.size() - 1));
         System.out.println("]");
     }
 
     private void print2DList(List<List<Integer>> my2DList) {
         System.out.print("[\n");
-        for(int i=0;i<my2DList.size()-1;i++){
+        for (int i = 0; i < my2DList.size() - 1; i++) {
             System.out.print("  ");
             printList(my2DList.get(i));
         }
         System.out.print("  ");
-        printList(my2DList.get(my2DList.size()-1));
+        printList(my2DList.get(my2DList.size() - 1));
         System.out.println("\n]");
     }
 
@@ -164,10 +162,9 @@ public class Player implements matchup.sim.Player {
         int gain = 0;
         for (int i = 0; i < myRound.size(); i++) {
             int diff = myRound.get(i) - opponentRound.get(i);
-            if(diff >= 3){
+            if (diff >= 3) {
                 gain++;
-            }
-            else if(diff<=-3) {
+            } else if (diff <= -3) {
                 gain--;
             }
         }
@@ -192,5 +189,62 @@ public class Player implements matchup.sim.Player {
             }
         }
         return permutations.get(maxIndex);
+    }
+
+    /*** learning strategy helper ***/
+
+    //return 1-11 skill frequency density from history
+    private List<Double> getFrequencyDensity(List<List<Integer>> history) {
+        List<Double> result = new ArrayList<>(11);
+        for (List<Integer> l : history) {
+            for (int x : l) {
+                result.set(x - 1, result.get(x - 1) + 1);
+            }
+        }
+        double sum = 0;
+        for (double x : result) {
+            sum += x;
+        }
+        //std norm
+        for (int i = 0; i < result.size(); i++) {
+            result.set(i, result.get(i) / sum);
+        }
+        return result;
+    }
+
+    //return 1-11 skill frequency density from history - stream in
+    private static List<Double> frequencyDensity;
+    private static int historyCount = 0;
+
+    private void updateFrequencyDensity(List<Integer> streamInDistribution) {
+        if (frequencyDensity == null) {
+            frequencyDensity = new ArrayList<>(11);
+            double sum = 0;
+            for (double x : streamInDistribution) {
+                sum += x;
+            }
+            //std norm
+            for (int i = 0; i < streamInDistribution.size(); i++) {
+                frequencyDensity.set(i, streamInDistribution.get(i) / sum);
+            }
+            historyCount = 1;
+        } else {
+            List<Integer> tmpFreq = new ArrayList<>(11);
+            for (int x : streamInDistribution) {
+                tmpFreq.set(x - 1, tmpFreq.get(x - 1) + 1);
+            }
+            //update
+            for (int i = 0; i < frequencyDensity.size(); i++) {
+                frequencyDensity.set(i, frequencyDensity.get(i) / (historyCount + 1) * historyCount + streamInDistribution.get(i) / (++historyCount * 15));
+            }
+        }
+    }
+
+    //return a counter strategy distribution given frequencyDensity
+    //greedy search: high freq counter first, low tie first then high win
+    private List<Integer> generateCounterDistribution(List<Double> frequencyDensity) {
+        int skills = 90;
+        List<Integer> result = new ArrayList<>(15);
+
     }
 }
